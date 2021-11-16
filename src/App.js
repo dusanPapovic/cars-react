670302588553393b63e4bd5a136462a57a3f2d7d
dusanPapovic <papovicdusan91@gmail.com>
2021-11-16 16:55:18 +0100

    cars app task 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AppCars from "./pages/AppCars";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <nav className="navbar-expand-lg navbar-light bg-light">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/cars">
+                  Cars
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/cars">
+            <AppCars />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/AppCars.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AppCars() {
+  return <div className="container">AppCars</div>;
+}
+
+export default AppCars;
